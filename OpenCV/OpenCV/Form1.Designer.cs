7a8c022a2c40f805614969fdd0e7f94d9d248657
UgurOymen2 <37594454+UgurOymen2@users.noreply.github.com>
2021-02-03 08:34:56 +0300

    p

---
+++ OpenCV/OpenCV/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace OpenCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorRListView = new System.Windows.Forms.ListView();
+            this.colorGListView = new System.Windows.Forms.ListView();
+            this.colorBListView = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 64);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ZigZag Donuştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // colorRListView
+            // 
+            this.colorRListView.HideSelection = false;
+            this.colorRListView.Location = new System.Drawing.Point(19, 123);
+            this.colorRListView.Name = "colorRListView";
+            this.colorRListView.Size = new System.Drawing.Size(201, 291);
+            this.colorRListView.TabIndex = 3;
+            this.colorRListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // colorGListView
+            // 
+            this.colorGListView.HideSelection = false;
+            this.colorGListView.Location = new System.Drawing.Point(245, 123);
+            this.colorGListView.Name = "colorGListView";
+            this.colorGListView.Size = new System.Drawing.Size(201, 291);
+            this.colorGListView.TabIndex = 4;
+            this.colorGListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // colorBListView
+            // 
+            this.colorBListView.HideSelection = false;
+            this.colorBListView.Location = new System.Drawing.Point(482, 123);
+            this.colorBListView.Name = "colorBListView";
+            this.colorBListView.Size = new System.Drawing.Size(201, 291);
+            this.colorBListView.TabIndex = 5;
+            this.colorBListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(81, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Color R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(305, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 22);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Color G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(544, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 22);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Color B";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::OpenCV.Properties.Resources.colour;
+            this.pictureBox1.Location = new System.Drawing.Point(156, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(245, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.colorBListView);
+            this.Controls.Add(this.colorGListView);
+            this.Controls.Add(this.colorRListView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView colorRListView;
+        private System.Windows.Forms.ListView colorGListView;
+        private System.Windows.Forms.ListView colorBListView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ OpenCV/OpenCV/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OpenCV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Color OkunanRenk, DonusenRenk;
+            int R = 0, G = 0, B = 0;
+            Bitmap GirisResmi, CikisResmi;
+            GirisResmi = new Bitmap(pictureBox1.Image);
+            int ResimGenisligi = GirisResmi.Width;
+            int ResimYuksekligi = GirisResmi.Height;
+            int[,] colorR = new int[ResimGenisligi, ResimYuksekligi];
+            int[,] colorG = new int[ResimGenisligi, ResimYuksekligi];
+            int[,] colorB = new int[ResimGenisligi, ResimYuksekligi];
+            CikisResmi = new Bitmap(ResimGenisligi, ResimYuksekligi);
+            for (int x = 0; x < ResimGenisligi; x++)
+            {
+                for (int y = 0; y < ResimYuksekligi; y++)
+                {
+                    OkunanRenk = GirisResmi.GetPixel(x, y);
+                    R = OkunanRenk.R;
+                    G = OkunanRenk.G;
+                    B = OkunanRenk.B;
+                    /*Burada Matris Dökülüyor*/
+                    colorR[x, y] = OkunanRenk.R;
+                    colorG[x, y] = OkunanRenk.G;
+                    colorB[x, y] = OkunanRenk.B;
+                    DonusenRenk = Color.FromArgb(R, G, B);
+                    CikisResmi.SetPixel(x, y, DonusenRenk);
+                }
+            }
+            pictureBox2.Image = CikisResmi;
+            var colorRstring = "";
+            var colorGstring = "";
+            var colorBstring = "";
+            for (int i = 0; i < ResimGenisligi; i++)
+            {
+                for (int j = 0; j < ResimYuksekligi; j++)
+                {
+                    colorRstring += colorR[i, j] + ",";
+                    colorGstring += colorG[i, j] + ",";
+                    colorBstring += colorB[i, j] + ",";
+                }
+            }
+            var colorRZigZagList = zigZagMatrix(colorR, ResimGenisligi, ResimYuksekligi);
+            var colorGZigZagList = zigZagMatrix(colorG, ResimGenisligi, ResimYuksekligi);
+            var colorBZigZagList = zigZagMatrix(colorB, ResimGenisligi, ResimYuksekligi);
+            foreach (var item in colorRZigZagList)
+            {
+                if (item != "0")
+                    colorRListView.Items.Add(item);
+            }
+            foreach (var item in colorGZigZagList)
+            {
+                if (item != "0")
+                    colorGListView.Items.Add(item);
+            }
+            foreach (var item in colorBZigZagList)
+            {
+                if (item != "0")
+                    colorBListView.Items.Add(item);
+            }
+        }
+
+        public static List<string> zigZagMatrix(int[,] arr, int n, int m)
+        {
+            int row = 0, col = 0;
+            //var zigzagMatrisstring = "";
+            List<string> zigzagmatris = new List<string>();
+            bool row_inc = false;
+
+            int mn = Math.Min(m, n);
+            for (int len = 1; len <= mn; ++len)
+            {
+                for (int i = 0; i < len; ++i)
+                {
+
+                    zigzagmatris.Add(arr[row, col].ToString());
+                    if (i + 1 == len)
+                        break;
+                    if (row_inc)
+                    {
+                        ++row;
+                        --col;
+                    }
+                    else
+                    {
+                        --row;
+                        ++col;
+                    }
+                }
+
+                if (len == mn)
+                    break;
+                if (row_inc)
+                {
+                    ++row;
+                    row_inc = false;
+                }
+                else
+                {
+                    ++col;
+                    row_inc = true;
+                }
+            }
+            if (row == 0)
+            {
+                if (col == m - 1)
+                    ++row;
+                else
+                    ++col;
+                row_inc = true;
+            }
+            else
+            {
+                if (row == n - 1)
+                    ++col;
+                else
+                    ++row;
+                row_inc = false;
+            }
+            int MAX = Math.Max(m, n) - 1;
+            for (int len, diag = MAX; diag > 0; --diag)
+            {
+
+                if (diag > mn)
+                    len = mn;
+                else
+                    len = diag;
+
+                for (int i = 0; i < len; ++i)
+                {
+                    zigzagmatris.Add(arr[row, col].ToString());
+                    if (i + 1 == len)
+                        break;
+                    if (row_inc)
+                    {
+                        ++row;
+                        --col;
+                    }
+                    else
+                    {
+                        ++col;
+                        --row;
+                    }
+                }
+                if (row == 0 || col == m - 1)
+                {
+                    if (col == m - 1)
+                        ++row;
+                    else
+                        ++col;
+
+                    row_inc = true;
+                }
+                else if (col == 0 || row == n - 1)
+                {
+                    if (row == n - 1)
+                        ++col;
+                    else
+                        ++row;
+
+                    row_inc = false;
+                }
+            }
+            return zigzagmatris;
+        }
+
+    }
+}
